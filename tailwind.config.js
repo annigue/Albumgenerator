9de536d42638d5df87ad7bd21099f9e466101012
Anni <39795105+annigue@users.noreply.github.com>
2025-10-15 18:29:56 +0200

    Refactor color definitions in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,32 +4,30 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  extend: {
-  colors: {
-    brandPink: {
-      400: "#ff7070",
-      500: "#ff4040",
-      600: "#cc3333",
-    },
-    brandOrange: {
-      400: "#ffc27a",
-      500: "#ffa54f",
-      600: "#e68e38",
-    },
-    brandlgreen: {
-      400: "#80e0b0",
-      500: "#66cdaa",
-      600: "#4ba885",
-    },
-    brandlightgrey: {
-      400: "#e0e0e0",
-      500: "#d3d3d3",
-      600: "#b0b0b0",
+    extend: {
+      colors: {
+        brandPink: {
+          400: "#ff7070",
+          500: "#ff4040",
+          600: "#cc3333",
+        },
+        brandOrange: {
+          400: "#ffc27a",
+          500: "#ffa54f",
+          600: "#e68e38",
+        },
+        brandlgreen: {
+          400: "#80e0b0",
+          500: "#66cdaa",
+          600: "#4ba885",
+        },
+        brandlightgrey: {
+          400: "#e0e0e0",
+          500: "#d3d3d3",
+          600: "#b0b0b0",
+        },
+      },
     },
   },
-},
-
-  },
-},
   plugins: [],
 };
