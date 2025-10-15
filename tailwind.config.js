086b93806c1fa26df87cf238226a0baa85fb954d
Anni <39795105+annigue@users.noreply.github.com>
2025-10-15 18:25:21 +0200

    Refactor brand colors to use shade objects
    Updated color definitions to use shades for brand colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,30 @@ module.exports = {
   ],
   theme: {
   extend: {
-    colors: {
-      brandPink: "#ff4040",
-      brandOrange: "#ffa54f",
-      brandlightgrey: "#d3d3d3",
-      brandlgreen: "#66cdaa",
+  colors: {
+    brandPink: {
+      400: "#ff7070",
+      500: "#ff4040",
+      600: "#cc3333",
     },
+    brandOrange: {
+      400: "#ffc27a",
+      500: "#ffa54f",
+      600: "#e68e38",
+    },
+    brandlgreen: {
+      400: "#80e0b0",
+      500: "#66cdaa",
+      600: "#4ba885",
+    },
+    brandlightgrey: {
+      400: "#e0e0e0",
+      500: "#d3d3d3",
+      600: "#b0b0b0",
+    },
+  },
+},
+
   },
 },
   plugins: [],
