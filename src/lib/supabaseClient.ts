a73a88d244a3c44beed826f78585017e25f64dee
annigue <deine@mail.de>
2025-12-21 00:35:48 +0100

    supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "@/types/supabase";
+import type { Database } from "../types/supabase";
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // = Publishable Key (sb_publishable_...)
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
