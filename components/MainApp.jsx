eaa90ece42e5f7434ecd2e8bb6e71439500722de
annigue <deine@mail.de>
2026-02-22 12:56:06 +0100

    layout

--- components/MainApp.jsx
+++ components/MainApp.jsx
@@ -363,9 +363,8 @@ export default function MainApp() {
               <div className="poster-sep" />
 
               {/* Bewertung */}
-              <div className="poster-label">ALBUM BEWERTEN</div>
               <section className="poster-block poster-block--form">
-                
+                <div className="poster-label">ALBUM BEWERTEN</div>
                 <BewertungForm album={currentAlbum} onSubmitted={loadAlbums} />
               </section>
 
@@ -373,10 +372,8 @@ export default function MainApp() {
 
               {/* Bisherige Alben + Micro */}
               <div className="poster-row">
-                <div className="poster-label">BISHERIGE ALBEN</div>
-               
                 <section className="poster-block poster-block--stats poster-block--stats-gray">
-                  
+                  <div className="poster-label poster-label--stats">BISHERIGE ALBEN</div>
                   {pastAlbums.length > 0 && selectedPast ? (
                     <>
                       {/* Cover + Stempel */}
@@ -477,16 +474,15 @@ export default function MainApp() {
                   <img src="/micro.png" alt="" className="poster-image" />
                 </section>
 
-              {/* Vorschlagen */}
-              <div className="poster-label">NEUES ALBUM VORSCHLAGEN</div>
-              <section className="poster-block poster-block--form">
-                <VorschlagForm />
-              </section>
+              </div>
 
               <div className="poster-sep" />
 
-             
-              </div>
+              {/* Vorschlagen */}
+              <section className="poster-block poster-block--form">
+                <div className="poster-label">NEUES ALBUM VORSCHLAGEN</div>
+                <VorschlagForm />
+              </section>
             </div>
           </div>
         </div>
