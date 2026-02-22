c10cc8fc5bba4ebc473034864dec7d15158689b7
annigue <deine@mail.de>
2026-02-22 12:13:28 +0100

    layout

--- components/MainApp.jsx
+++ components/MainApp.jsx
@@ -286,7 +286,7 @@ export default function MainApp() {
       <div className="pattern-top" />
 
       <div className="content-bg">
-        <div className="max-w-4xl mx-auto p-8 relative z-10">
+        <div className="w-full px-8 relative z-10">
           <div className="poster">
             <header className="poster-header">
               <img
